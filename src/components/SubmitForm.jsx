56f74e581fe2a91aaada38c0935ed1844e5c00f9
amir.mofid@apsaaz.com <amir.mofid@apsaaz.com>
2023-05-09 11:38:26 +0430

    8

--- src/components/SubmitForm.jsx
+++ src/components/SubmitForm.jsx
@@ -25,24 +25,24 @@ const SubmitForm = () =>{
           
           </div>
           <div class="mb-3">
-              <label class="block text-gray-700 text-sm font-bold my-4" for="password">
+              <label class="block text-gray-700 text-sm font-bold mt-4 mb-2 font-IRsans" for="password">
                 موضوع سفر
               </label>
-              <input class="shadow appearance-none border text-sm border-[#CCCCCD] rounded w-[40%] py-2 px-3 text-[#CCCCCD] mb-3 leading-tight focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="password" type="text" placeholder="موضوع سفر خود را اینجا بنویسید..."/>
+              <input class="shadow appearance-none border text-sm font-IRsans border-[#CCCCCD] rounded w-[40%] py-2 px-3 text-[#CCCCCD] mb-3 leading-tight focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="password" type="text" placeholder="موضوع سفر خود را اینجا بنویسید..."/>
       
           </div> 
           <div class="mb-6">
-              <label class="block text-gray-700 text-sm font-bold my-4" for="password">
+              <label class="block text-gray-700 text-sm font-bold mt-4 mb-2 font-IRsans" for="password">
                 توضیحات خود راجع به سفر٬دستاوردها و اتفاقات سفر را بنویسید.
               </label>
-              <textarea id="message" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded border  border-[#CCCCCD] focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="توضیحات خود را اینجا بنویسید..."></textarea>
+              <textarea id="message" rows="8" class="block p-2.5 font-IRsans w-full text-sm text-gray-900 bg-gray-50 rounded border  border-[#CCCCCD] focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="توضیحات خود را اینجا بنویسید..."></textarea>
           </div>
           <div class="flex items-center justify-start w-full">
         <label for="dropzone-file" class="flex flex-col items-center justify-center w-[40%] h-64 border-2 border-[#CCCCCD] border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            <div class="flex flex-col items-center justify-center pt-5 pb-6 font-IRsans">
                 <svg aria-hidden="true" class="w-10 h-10 mb-3 text-[#B7B8BB]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">انتخاب فایل</span></p>
-                <p class="text-xs text-gray-500 dark:text-gray-400">JPG,JPEG,PNG,PDF,TXT</p>
+                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold font-IRsans">انتخاب فایل</span></p>
+                <p class="text-xs text-gray-500 dark:text-gray-400 font-IRsans">JPG,JPEG,PNG,PDF,TXT</p>
             </div>
             <input id="dropzone-file" type="file" class="" />
         </label>
--- src/pages/UserPanel/SubmitReport.jsx
+++ src/pages/UserPanel/SubmitReport.jsx
@@ -3,7 +3,7 @@ import AgentRightMenu from "../../components/AgentRightMenu";
 import SubmitForm from "../../components/SubmitForm";
 const SubmitReport = () => {
     return(
-        <div className="w-full h-screen bg-lightGray py-10 px-32 " style={{direction:'rtl'}}>
+        <div className="w-full h-full bg-lightGray py-10 px-32 " style={{direction:'rtl'}}>
             <div className="flex">
                 <AgentRightMenu/>
                 <div className="bg-white rounded-tr-none rounded-br-none rounded-tl-2xl rounded-bl-2xl p-12 my-10 w-[80%]">
