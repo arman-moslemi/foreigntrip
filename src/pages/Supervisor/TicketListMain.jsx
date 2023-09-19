70dc8e7fd642968b5d691dcb143871735c800f98
arman_moslemi <hdsarman@gmail.com>
2023-09-19 18:24:47 +0330

    28/6

--- src/pages/Supervisor/TicketListMain.jsx
+++ src/pages/Supervisor/TicketListMain.jsx
@@ -30,7 +30,7 @@ const SupervisorTicketList= () => {
                   <div
                    className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                  >
-                   <div className="relative w-[30%] my-5 mx-auto max-w-5xl md:w-[90%]">
+                   <div className="relative w-[30%] xl-lg:w-[50%] my-5 mx-auto max-w-5xl md:w-[90%] xs:w-[96%]">
                     
                      <div className="border-0 rounded-lg p-2 shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                   
